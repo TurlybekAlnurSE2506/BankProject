66c681fa7ad35ae8134520f0102a014bb97221a6
user <29045@astanait.edu.kz>
2026-02-04 15:02:22 +0500

    Implemented REST for Customer

--- src/main/java/Service/CustomerService.java
+++ src/main/java/Service/CustomerService.java
@@ -1,4 +1,18 @@
 package Service;
 
+import entities.Customer;
+import repositories.CustomerRepo;
+
+import java.util.List;
+
 public class CustomerService {
+    private final CustomerRepo customerRepo;
+
+    public CustomerService(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepo.findAll();
+    }
 }
--- src/main/java/controllers/CustomerController.java
+++ src/main/java/controllers/CustomerController.java
@@ -1,4 +1,24 @@
 package controllers;
 
+import entities.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import repositories.CustomerRepo;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+    private final CustomerRepo customerRepo;
+
+    public CustomerController(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerRepo.findAll();
+    }
 }
